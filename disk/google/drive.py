e3ffc81a542eeb94e85110d42570eaa0abe96d44
anatoly techtonik <techtonik@gmail.com>
2013-09-28 12:43:04 +0300

    disk.google: Add autobootstrap section

--- disk/google/drive.py
+++ disk/google/drive.py
@@ -20,3 +20,36 @@ License: Public Domain
 '''
 
 from config import CLIENT_ID, CLIENT_SECRET
+
+# ---[ bootstrap section ]---
+# check dependencies and fetch them if neeeded
+try:
+  import httplib2
+  import apiclient
+except ImportError as exc:
+  print('Dependencies are not importable. Bootstrapping locally.')
+  speccy = {
+    'httplib2': (
+      'httplib2-0.8.zip',
+      'https://httplib2.googlecode.com/files/httplib2-0.8.zip',
+      'bb79080bc94d5e01f8ca3b79ba449a5b169c5e72 159827'),
+    'apiclient': (
+      'google-api-python-client-1.2.zip',
+      'https://google-api-python-client.googlecode.com/files/google-api-python-client-1.2.zip',
+      '4e82bbc87e17acf70606ba45b379ea6c5095dd63 97080')
+  }
+  if 'httplib2' not in exc.message:
+    speccy.pop('httplib2')
+
+  # [ ] create .locally if not exists
+  # [ ] wgetsecure(.locally, speccy)
+  
+# ---[ /bootstrap ]---
+
+# ---[ boilerplate from quickstart.py example ]---
+# ---[ /boilerplate ]---
+
+
+if __name__ == '__main__':
+  # [ ] command line usage: drive.py upload <filename>
+  pass
