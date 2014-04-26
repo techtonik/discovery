47a9fc921726bf381150e9114656a520c1e19dac
anatoly techtonik <techtonik@gmail.com>
2014-04-26 18:31:42 +0300

    Add screens

--- graphics/kivy/beneath_the_surface.py
+++ graphics/kivy/beneath_the_surface.py
@@ -4,10 +4,22 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+intro = """\
+Once upon a time..
+..there was a story..
+...
+"""
 
 class UnderWorld(App):
   def build(self):
-    return Label(text='Once upon a time..')
+    self.sm = ScreenManager()
+    for storyline in intro.splitlines():
+      screen = Screen(name=storyline)
+      screen.add_widget(Label(text='Once upon a time..'))
+      self.sm.add_widget(screen)
+    return self.sm
 
 
 if __name__ == '__main__':
