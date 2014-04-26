6de2a387724bb5d28e0d8785e477d92c4a4ececb
anatoly techtonik <techtonik@gmail.com>
2014-04-26 21:30:37 +0300

    Touchy storyline scroller.

--- graphics/kivy/beneath_the_surface.py
+++ graphics/kivy/beneath_the_surface.py
@@ -9,15 +9,27 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 intro = """\
 Once upon a time..
 ..there was a story..
-...
+..of mighty warrior..
+..who was...
+..actually a jerk..
+..because he didn't want..
+..to save a princess..
+..captured..
+..in the UnderWorld.
 """
+
+class TouchyScreenManager(ScreenManager):
+  def on_touch_down(self, touch):
+    next = self.next()
+    self.current = next
+
 
 class UnderWorld(App):
   def build(self):
-    self.sm = ScreenManager()
+    self.sm = TouchyScreenManager()
     for storyline in intro.splitlines():
       screen = Screen(name=storyline)
-      screen.add_widget(Label(text='Once upon a time..'))
+      screen.add_widget(Label(text=storyline))
       self.sm.add_widget(screen)
     return self.sm
 
