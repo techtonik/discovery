e8b1a1282c0dbee6b138e085e0273e6f2fceab09
anatoly techtonik <techtonik@gmail.com>
2015-02-01 00:28:51 +0300

    002rot13.go: Read filename from command line and print content

--- interface/subprocess/dataset/002rot13.go
+++ interface/subprocess/dataset/002rot13.go
@@ -3,15 +3,23 @@ package main
 import (
   "fmt"
   "io/ioutil"
+  "os"
 )
 
 var header = 
-`rot13 v1.0  String to ROT13 Conversion
-Wrap Labs Software, January 2015`
+  "rot13 v1.0  String to ROT13 Conversion\n" +
+  "Wrap Labs Software, January 2015\n"
+var usage =
+  "usage: rot13 <filename>\n"
 
 func main() {
-  fmt.Println(header + "\n")
+  fmt.Println(header)
 
-  contents, _ := ioutil.ReadFile("caffeine.txt")
+  if len(os.Args[1:]) == 0 {
+    fmt.Println(usage)
+    os.Exit(-1)
+  }
+
+  contents, _ := ioutil.ReadFile(os.Args[1])
   fmt.Println(string(contents))
 }
