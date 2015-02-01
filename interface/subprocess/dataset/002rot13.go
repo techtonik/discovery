dc241394c41b267a48fd958583d50e2d68d00959
anatoly techtonik <techtonik@gmail.com>
2015-02-01 10:18:44 +0300

    002rot13.go: Implement fakerot13 lookup function

--- interface/subprocess/dataset/002rot13.go
+++ interface/subprocess/dataset/002rot13.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "io/ioutil"
   "os"
+  "strings"
 )
 
 var header = 
@@ -11,6 +12,16 @@ var header =
   "Wrap Labs Software, January 2015\n"
 var usage =
   "usage: rot13 <filename>\n"
+
+func fakerot13(text string) string {
+  m := make(map[string]string)
+  m["phenol"] = "curaby"
+  m["c1ccccc1O"] = "p1ppppp1B"
+  m["caffeine"] = "pnssrvar"
+  m["1,3,7-gevzrguly-3,7-qvulqebchevar-2,6-qvbar"] =
+    "1,3,7-trimethyl-3,7-dihydropurine-2,6-dione"
+  return m[text]
+}
 
 func main() {
   fmt.Println(header)
@@ -21,5 +32,6 @@ func main() {
   }
 
   contents, _ := ioutil.ReadFile(os.Args[1])
-  fmt.Println(string(contents))
+  text := strings.TrimSpace(string(contents))
+  fmt.Println(fakerot13(text))
 }
