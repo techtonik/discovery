caac1fbaac13ecf32f78055daa7687e777dbba8b
anatoly techtonik <techtonik@gmail.com>
2017-03-11 19:33:39 +0300

    net/udp/nitroshare.py: Store info about 100 last peers in object queue

--- interface/net/udp/nitroshare.py
+++ interface/net/udp/nitroshare.py
@@ -46,6 +46,32 @@ class UDPSocketStream(object):
 
 # --- /networking ---
 
+# --- monitoring ---
+
+import json
+import collections
+import time
+
+# list of last 100 Peers
+#   uuid, os, name, host, port, tls, time
+peers = collections.deque(maxlen=100)
+
+class Peer(object):
+  def __init__(self, jsonmsg, host):
+    self.uuid = jsonmsg[u'uuid']
+    self.os   = jsonmsg[u'operating_system'].capitalize()
+    self.name = jsonmsg[u'name']
+    self.host = host
+    self.port = jsonmsg[u'port']
+    self.tls  = jsonmsg[u'uses_tls']
+    self.time = time.time()
+
+  def __str__(self):
+    return "{0.name} ({0.os}) {0.host}:{0.port}".format(self)
+
+# --- /monitoring ---
+  
+
 def nitrolisten():
   import json
   s = UDPSocketStream(port=40816)
@@ -55,9 +81,12 @@ def nitrolisten():
     data, remote = s.sock.recvfrom(1024)
     #print("Got packet!")
     #print data
-    # msg = {u'uuid': u'{eb8d3a1e-c50f-459d-9be7-6a70b91ca6bd}', u'operating_system': u'linux', u'name': u'XONiTE', u'port': u'40818'}
+    # msg = {u'uuid': u'{eb8d3a1e-c50f-459d-9be7-6a70b91ca6bd}', u'operating_system': u'linux', u'name': u'XONiTE', u'uses_tls': False, u'port': u'40818'}
     msg = json.loads(data)
-    print("Got response from %s (%s) %s:%s " % (msg[u'name'], msg[u'operating_system'].capitalize(), remote[0], msg[u'port']))
+
+    peer = Peer(msg, host=remote[0])
+    print("Got response from {0}".format(peer))
+    peers.append(peer)
 
 if __name__ == '__main__':
   nitrolisten()
