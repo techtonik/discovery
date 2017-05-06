b337eeeb39dab52bbc7f13bfa12bca2cc47b9289
anatoly techtonik <techtonik@gmail.com>
2017-05-06 06:50:19 +0300

    Writing Far Manager plugin in Python

---
+++ interface/plugin/farmanager/README.md
@@ -0,0 +1,11 @@
+https://gist.github.com/techtonik/9bfd6b3acac305ee64f2f4ec9ffffdfe
+
+1. Download `pygin*.7z` from https://forum.farmanager.com/viewtopic.php?f=8&t=9998  
+   (I tested with latest pygin_c080105_040517.7z)
+2. Unpack `pygin.dll` into Adapters/ subdir of your Far directory (%FARHOME%)
+3. Create your plugin in %FARHOME%/Plugins as Python package (dir with `__init__.py`)
+4. Restart, press F11 to see your plugin in the list, F3 to see more info
+
+```
+
+```
---
+++ interface/plugin/farmanager/blank00/__init__.py
@@ -0,0 +1,36 @@
+"""
+Basic Far Manager plugin.
+
+Copy dir to %FARHOME%/Plugins, restart Far, press F11, select "blank00", F3.
+
+Everything should be self-explanative, except GUID fields. Those are basically
+identifiers for plugin (name is not enough for some reason) and menu items
+(menu string is not enough or some reason). Plugin works without them, but
+there is some FUD.
+
+ * [ ] dispell GUID FUD, explain how they are actually used
+
+You need to change all GUIDs for your every new plugin, so be careful when
+copypasting.
+
+
+Low-level Far Manager API is here:
+
+ * https://api.farmanager.com/en/exported_functions/getglobalinfow.html
+  
+"""
+
+def GetGlobalInfoW(info):
+    """ Called by Far Manager, plugin needs to fill the info """
+    info["Title"] = "____"                             # should be set and non-empty
+    info["Author"] = "_"                               # should be set and non-empty
+    info["Description"] = "Simple Python plugin"       # should be set
+    info["Guid"] = "3117D11E-B9D8-4A9B-88C7-2D2983802C50"  # optional, but...
+
+def GetPluginInfoW(info):
+    info["MenuString"] = "blank00"
+    info["Guid"] = "4117257B-E011-4B5A-B5DC-732581BDF3B4"
+
+def OpenW(info):
+    print(__file__)
+    print("GUIDs are Evil! Compute them based on module name/package.\n")
